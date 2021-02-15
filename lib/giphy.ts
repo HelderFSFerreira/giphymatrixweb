e5faf2f79bcef45ae3176498b9f5ceaadcabe28e
Helder Ferreira <me@helderferreira.io>
2021-02-15 08:31:48 +0000

    Fix url error

--- lib/giphy.ts
+++ lib/giphy.ts
@@ -11,8 +11,7 @@ export class GiphyAPI {
     }
 
     async getBest5(word: string): Promise<object> {
-        const url = `${encodeURIComponent(GiphyAPI.apiURL)}?api_key=${encodeURIComponent(GiphyAPI.apiKey)}&q=${encodeURIComponent(word)}&limit=5&offset=0&rating=g&lang=en`;
-        
+        const url = `${GiphyAPI.apiURL}?api_key=${encodeURIComponent(GiphyAPI.apiKey)}&q=${encodeURIComponent(word)}&limit=5&offset=0&rating=g&lang=en`;
         const response = await fetch(url);
         const data = await response.json();
         const imgUrl: GiphyAPIOut[] = data.data.map((gitLine:any) => {
